038d7351e01d25361da6c71354e573ae0ac49ef9
Bruno de Macedo Sanagiotto <62435631+brunodms@users.noreply.github.com>
2022-06-27 11:33:50 -0300

    v1

--- D_Binomial_Bruno.py
+++ D_Binomial_Bruno.py
@@ -1,11 +1,13 @@
-from scipy.stats import binom
+from scipy.stats import binom 
 
-n = float(input("N° de observações: "))
+n = float (input ("N° de observações: ")) 
 
-p = float(input("Probabilidade de sucesso: "))
+p = float (input ("Probabilidade de sucesso: ")) 
 
-x = int(input("Quantidades de sucessos: "))
+x = int (input ("Quantidades de sucessos: ")) 
 
-p_x = binom.pmf(x, n, p)
- 
-print("P(x) = ", p_x)
+pbi = binom.pmf (x, n, p)
+pba = binom.cdf(x, n, p)
+
+print ("Probabilidade binomial individual = ", format(pbi, ".4f"))
+print ("Probabilidade binomial acumulada = ",  format(pba))
